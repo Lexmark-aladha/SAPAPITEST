4997a1d488eee21b82c119f1c4fcfaea79be3414
Sohini Paul <sohini.paul@lexmark.com>
2023-01-16 20:44:18 +0530

    Final Changes GL Account Balance Sandbox

--- sapapi/src/test/java/com/lexmark/GLAccountBalancesECX1.java
+++ sapapi/src/test/java/com/lexmark/GLAccountBalancesECX1.java
@@ -1,3 +1,12 @@
+
+/**
+ * @author Sohini Paul
+
+ *
+ * @date 01/16/2022
+ * 
+ * @description	GLAccountBalancesECX1 
+ */
 package com.lexmark;
 
 import java.io.FileReader;
@@ -14,60 +23,55 @@ import io.restassured.response.Response;
 
 public class GLAccountBalancesECX1 {
 
-	
-	static String x_csrf_token2, Cookies;
+	static String x_csrf_token, Cookies;
 	static String[] outputData;
 	static Properties input = new Properties();
 	static FileReader readerInput;
 	static FileWriter writerOutput;
 	static CSVWriter writer;
 
-	@Test	
+	@Test
 	public void GLAccountBalancesTestECX1() {
-		
-		
+
 		TS01GetAccountBalances();
-		
+
 	}
 
-    @Test	
+	@Test
 	public void TS01GetAccountBalances() {
-		
+
 		try {
 			readerInput = new FileReader("data/InputData.properties");
 			input.load(readerInput);
 
-			/*
-			 * writerOutput = new FileWriter("output/GLAccountBalancesECX1.csv", false);
-			 * writer = new CSVWriter(writerOutput);
-			 */
-		   String ledger=input.getProperty("GLAccountBalance.Ledger");
-		   String companyCode=input.getProperty("GLAccountBalance.CompanyCode");
-		   String ledgerFiscalYear=input.getProperty("GLAccountBalance.LedgerFiscalYear");
-		   
+			writerOutput = new FileWriter("output/GLAccountBalancesECX1.csv", false);
+			writer = new CSVWriter(writerOutput);
+
+			String ledger = input.getProperty("GLAccountBalance.Ledger");
+			String companyCode = input.getProperty("GLAccountBalance.CompanyCode");
+			String ledgerFiscalYear = input.getProperty("GLAccountBalance.LedgerFiscalYear");
+
 			Response response = RestAssured.given()
 					.baseUri("https://vhlxkecxci.hec.lexmark.com:44300/sap/opu/odata/sap/FAC_GL_ACCOUNT_BALANCE_SRV")
 					.relaxedHTTPSValidation().auth().preemptive()
-					.basic(input.getProperty("GLAccountBalance.User"),
-							input.getProperty("GLAccountBalance.Password"))
-					.header("x-csrf-token", "Fetch")
-					.queryParam("$format", "json")
-					.queryParam("$top", "99")
-					.when().get("/GL_ACCOUNT_BALANCESet?$select=LedgerFiscalYear, LedgerFiscalPeriod,IsNotPostedTo, CreditAmountInCoCodeCrcy, DebitAmountInCompanyCodeCrcy, BalAmtInCompanyCodeCrcy, AccmltdBalAmtInCoCodeCrcy, CompanyCodeCurrency&$filter=Ledger eq '"+ledger+"' and CompanyCode eq '"+companyCode+"' and LedgerFiscalYear eq '"+ledgerFiscalYear+"'&")
-					.then().statusCode(200).statusLine("HTTP/1.1 200 OK")
-					.log().all()
-					.extract().response();
-			
-			
-		//	Assert.assertTrue(response.jsonPath().get("d.ApproveDRFBRequest.Success"));
-		/*
-		 * outputData = new String[] { "GL Account Balances", "Passed"};
-		 * 
-		 * writer.writeNext(outputData, false); writer.close();
-		 */
-	
-		}catch(Exception e) {
+					.basic(input.getProperty("GLAccountBalance.User"), input.getProperty("GLAccountBalance.Password"))
+					.header("x-csrf-token", "Fetch").queryParam("$format", "json").queryParam("$top", "99").when()
+					.get("/GL_ACCOUNT_BALANCESet?$select=LedgerFiscalYear, LedgerFiscalPeriod,IsNotPostedTo, CreditAmountInCoCodeCrcy, DebitAmountInCompanyCodeCrcy, BalAmtInCompanyCodeCrcy, AccmltdBalAmtInCoCodeCrcy, CompanyCodeCurrency&$filter=Ledger eq '"
+							+ ledger + "' and CompanyCode eq '" + companyCode + "' and LedgerFiscalYear eq '"
+							+ ledgerFiscalYear + "'&")
+					.then().statusCode(200).statusLine("HTTP/1.1 200 OK").log().all().extract().response();
+
+			for (int i = 0; i < response.jsonPath().getList("d.results").size(); i++) {
+				String period = response.jsonPath().get("d.results[" + i + "].LedgerFiscalPeriod");
+
+			}
+
+			outputData = new String[] { "GL Account Balances", "Passed" };
+			writer.writeNext(outputData, false);
+			writer.close();
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-}
+	}
 }
